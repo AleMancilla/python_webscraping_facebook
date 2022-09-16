94439207e07e601f80a59eb0def1bb5bc81c438f
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2022-09-16 14:01:52 -0400

    facebook scraping funcionando

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hola Mundo"
+    return "Hola Mundoasdasdadd"
 
 
 @app.route('/web_scraping/facebook_page/<url_page>')
@@ -35,5 +35,5 @@ def web_scraping_facebook_page_and_nro_post(url_page,nro_posts):
     return posts
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, port=5000)
 
